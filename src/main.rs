fa96526e149885d7c082511a850882b8cabd9a48
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2025-12-23 13:00:45 +0530

    learned fib series

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-fn is_even(n:i32)->bool{
-    if n%2 == 0 {
-        return true;
-    }
-    else{
-        return false;
-    }
-}
+// 1
+
+// fn is_even(n:i32)->bool{
+//     if n%2 == 0 {
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// fn main(){
+//     println!("{}",is_even(31))
+// }
+
+// 2
+
 fn main(){
-    println!("{}",is_even(31))
+    let _x = 4;
+    println!("{}",fib(3));
+}
+
+fn fib(num:i32) -> i32 {
+    let mut first =0;
+    let mut second =1;
+    if num==0{
+        return first;
+    }
+    if num==1{
+        return second;
+    }
+
+    for _ in 0..num-1{
+        let temp = second;
+        second= second+first;
+        first= temp;
+    }
+    return second;
 }
