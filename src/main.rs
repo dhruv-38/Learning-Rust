3febe034d1fe817ae49ae21992d55b7aca0dd8d1
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2025-12-23 13:24:01 +0530

    structs implemetation

--- src/main.rs
+++ src/main.rs
@@ -52,17 +52,46 @@
 
 //4
 
-struct User{
-    first_name:String,
-    last_name:String,
-    age: u16,
+// struct User{
+//     first_name:String,
+//     last_name:String,
+//     age: u16,
+// }
+
+// fn main(){
+//     let user =User{
+//         first_name: String::from("Dhruv"),
+//         last_name:String::from("Choudhary"),
+//         age: 20,
+//     };
+//     println!("{}",user.age);
+// }
+
+struct Rect {
+    width: i32,
+    height: i32,
+}
+
+impl Rect{
+    fn area(&self)-> i32{
+        self.width * self.height
+    }
+
+    fn perimeter(&self, num: i32) -> i32{
+        2 * (self.width * self.height)
+    }
+
+    fn debug() -> i32 {
+        return 1;
+    }
 }
 
 fn main(){
-    let user =User{
-        first_name: String::from("Dhruv"),
-        last_name:String::from("Choudhary"),
-        age: 20,
+    let rect1 = Rect{
+        width : 10,
+        height: 20,
     };
-    println!("{}",user.age);
+    println!("area is {}",rect1.area());
+    println!("perimeter is {}",rect1.perimeter(1));
+    println!("debug is {}",Rect::debug())
 }
