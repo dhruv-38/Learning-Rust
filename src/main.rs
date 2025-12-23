e474b408411724a543b6c795c1d3d90e316c8421
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2025-12-23 13:48:45 +0530

    option enum

--- src/main.rs
+++ src/main.rs
@@ -116,22 +116,44 @@
 
 //6.2
 
-enum Shape{
-    Rectangle(f64,f64),
-    Circle(f64),
+// enum Shape{
+//     Rectangle(f64,f64),
+//     Circle(f64),
+// }
+
+// fn main(){
+//     let rect = Shape::Rectangle(1.0, 2.0);
+//     calculate_area(rect);
+//     let cicle = Shape::Circle(1.0);
+//     calculate_area(cicle);
+// }
+
+// fn calculate_area(shape:Shape)-> f64{
+//     let area = match shape{
+//         Shape::Rectangle(a,b )=> a*b,
+//         Shape::Circle(r)=> 3.14*r*r,
+//     };
+//     return area;
+// }
+
+
+//7 Option enum
+
+fn main() {
+    let index = find_first_a(String::from("preet"));
+
+    match index {
+        Some(value) => println!("index is {}", value),
+        None => println!("a not found"),
+    }
 }
 
-fn main(){
-    let rect = Shape::Rectangle(1.0, 2.0);
-    calculate_area(rect);
-    let cicle = Shape::Circle(1.0);
-    calculate_area(cicle);
-}
+fn find_first_a(s: String) -> Option<i32> {
+    for (index, char) in s.chars().enumerate() {
+        if char == 'a' {
+            return Some(index as i32);
+        }
+    }
 
-fn calculate_area(shape:Shape)-> f64{
-    let area = match shape{
-        Shape::Rectangle(a,b )=> a*b,
-        Shape::Circle(r)=> 3.14*r*r,
-    };
-    return area;
+    return None;
 }
