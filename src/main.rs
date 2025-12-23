f1f51747bfd3bd71993ce53f633b3a9cda1dec98
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2025-12-23 13:59:40 +0530

    result enum

--- src/main.rs
+++ src/main.rs
@@ -139,21 +139,38 @@
 
 //7 Option enum
 
-fn main() {
-    let index = find_first_a(String::from("preet"));
+// fn main() {
+//     let index = find_first_a(String::from("preet"));
 
-    match index {
-        Some(value) => println!("index is {}", value),
-        None => println!("a not found"),
-    }
+//     match index {
+//         Some(value) => println!("index is {}", value),
+//         None => println!("a not found"),
+//     }
+// }
+
+// fn find_first_a(s: String) -> Option<i32> {
+//     for (index, char) in s.chars().enumerate() {
+//         if char == 'a' {
+//             return Some(index as i32);
+//         }
+//     }
+
+//     return None;
+// }
+
+//8 Result enum
+
+use std::fs::read_to_string;
+
+fn main() {
+    let ans = read_from_file_dhruv(String::from("a.txt"));
 }
 
-fn find_first_a(s: String) -> Option<i32> {
-    for (index, char) in s.chars().enumerate() {
-        if char == 'a' {
-            return Some(index as i32);
-        }
-    }
 
-    return None;
+fn read_from_file_dhruv(file_path: String) -> Result<String, String> {
+    let result = read_to_string(file_path); // Result
+    match result {
+        Ok(data) => Ok(data),
+        Err(err) => Err(String::from("File not read")),
+    }
 }
