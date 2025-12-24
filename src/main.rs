b15f1723a1d11de21f71422be79a1a60c6679d9b
Dhruv Choudhary <dhruvchoudhary38@gmail.com>
2025-12-24 13:58:17 +0530

    some package management

--- src/main.rs
+++ src/main.rs
@@ -160,17 +160,27 @@
 
 //8 Result enum
 
-use std::fs::read_to_string;
+// use std::fs::read_to_string;
 
-fn main() {
-    let ans = read_from_file_dhruv(String::from("a.txt"));
-}
+// fn main() {
+//     let ans = read_from_file_dhruv(String::from("a.txt"));
+// }
 
 
-fn read_from_file_dhruv(file_path: String) -> Result<String, String> {
-    let result = read_to_string(file_path); // Result
-    match result {
-        Ok(data) => Ok(data),
-        Err(err) => Err(String::from("File not read")),
-    }
-}
+// fn read_from_file_dhruv(file_path: String) -> Result<String, String> {
+//     let result = read_to_string(file_path); // Result
+//     match result {
+//         Ok(data) => Ok(data),
+//         Err(err) => Err(String::from("File not read")),
+//     }
+// }
+
+
+// 9 Package Management
+
+// use chrono::{Local};
+
+// fn main(){
+//     let now= Local::now();
+//     print!("current time is {}",now);
+// }
